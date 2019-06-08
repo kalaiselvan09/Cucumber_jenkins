4918688fd4ebfe02ce099cdfb03f926332aaffe7
kalaiselvan09 <kalaiselvanrk09@gmail.com>
2019-06-08 13:30:05 +0530

    New Cucumber Jenkins

--- CucumberTest1/src/test/java/org/Step_Definition/TestRun.java
+++ CucumberTest1/src/test/java/org/Step_Definition/TestRun.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features = "src/test/resources/Feature", tags= {"@us2"},monochrome=true, dryRun = false, glue = "org.Step_Definition", plugin = "html:target")
+@CucumberOptions(features = "src/test/resources/Feature",monochrome=true, dryRun = false, glue = "org.Step_Definition", plugin = {"json:target/report.json","html:target"})
 public class TestRun {
 
 }
